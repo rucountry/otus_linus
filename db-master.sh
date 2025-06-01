9cbe5d6b3622dc055e94a44dcdbaa7030897ce8c
Rasim <rasim_suleimanov@mail.ru>
2025-06-01 17:01:11 +0000

    db-master и prometheus

---
+++ db-master.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+MYSQL_CONFIG="/etc/mysql/mysql.conf.d/mysqld.cnf"
+APP_USER="app_user"                     # Пользователь для приложения
+APP_PASS="123"                          # Пароль пользователя приложения
+REPL_USER="repl_user"                   # Пользователь для репликации
+REPL_PASS="123"                         # Пароль пользователя для репликации
+
+user_exists() {
+   sudo  mysql -Nse "SELECT EXISTS(SELECT 1 FROM mysql.user WHERE User='$1' AND Host='%')" 2>/dev/null
+}
+
+
+#Установка MySQL
+echo "Обновление пакетов..."
+sudo apt update -qq
+if ! command -v mysql &> /dev/null; then
+sudo apt install mysql-server-8.0 -y
+sudo systemctl enable mysql
+fi
+
+#Версия mysql
+echo "MySQL version: $(mysql --version)"
+
+#Настройки MySQL
+cat <<'EOL' | sudo tee "$MYSQL_CONFIG" > /dev/null
+[mysqld]
+user                    = mysql
+bind-address            = 0.0.0.0
+key_buffer_size         = 16M
+myisam-recover-options  = BACKUP
+log_error               = /var/log/mysql/error.log
+server-id               = 1
+max_binlog_size         = 100M
+gtid-mode               = ON
+enforce-gtid-consistency
+log-replica-updates
+EOL
+echo 'Конфигурация mysql настроена'
+
+sudo systemctl restart mysql
+sudo systemctl status mysql
+
+#Пользователь для приложения
+
+if [ "$(user_exists "$APP_USER")" -eq 0 ]; then
+    echo "Создаю пользователя: $APP_USER"
+   sudo mysql <<MYSQL_SCRIPT
+        CREATE USER '$APP_USER'@'%' IDENTIFIED BY '$APP_PASS';
+        GRANT ALL PRIVILEGES ON *.* TO '$APP_USER'@'%' WITH GRANT OPTION;
+MYSQL_SCRIPT
+else
+    echo "Пользователь $APP_USER уже существует."
+fi
+
+#Пользователь для репликации
+if [ "$(user_exists "$REPL_USER")" -eq 0 ]; then
+    echo "Создаю пользователя: $REPL_USER"
+   sudo mysql <<MYSQL_SCRIPT
+        CREATE USER '$REPL_USER'@'%' IDENTIFIED BY '$REPL_PASS';
+        GRANT REPLICATION SLAVE ON *.* TO '$REPL_USER'@'%';
+MYSQL_SCRIPT
+else
+    echo "Пользователь $REPL_USER уже существует."
+fi
+
---
+++ prometheus.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+PROMETHEUS_CONFIG="/etc/prometheus/prometheus.yml"
+
+# Обновление пакетов
+echo "Обновление пакетов..."
+sudo apt-get update -qq
+
+
+if ! command -v prometheus &> /dev/null; then
+ sudo apt-get install -y prometheus
+ echo 'Prometheus установлен.'
+fi
+ 
+
+cat <<'EOL' | sudo tee "$PROMETHEUS_CONFIG" > /dev/null
+global:
+  scrape_interval:     15s # Set the scrape interval to every 15 seconds. Default is every 1 minute.
+  evaluation_interval: 15s # Evaluate rules every 15 seconds. The default is every 1 minute.
+  external_labels:
+      monitor: 'example'
+alerting:
+  alertmanagers:
+  - static_configs:
+    - targets: ['localhost:9093']
+rule_files:
+scrape_configs:
+  - job_name: 'prometheus'
+    scrape_interval: 5s
+    scrape_timeout: 5s
+    static_configs:
+      - targets: ['localhost:9090']
+
+  - job_name: node
+    static_configs:
+      - targets: ['192.168.31.85:9100','192.168.31.221:9100','192.168.31.137:9100']
+EOL
+echo 'Конфигурация prometheus настроена'
+sudo systemctl restart prometheus
+sudo systemctl status prometheus
+
+
+#GRAFANA
+
+if ! command -v grafana &> /dev/null; then
+ sudo apt install -y adduser libfontconfig1
+ sudo dpkg -i grafana_10.0.3_amd64.deb
+ echo 'Grafana установлена'
+fi
+ 
+sudo systemctl restart grafana-server.service
+sudo systemctl status grafana-server.service 
